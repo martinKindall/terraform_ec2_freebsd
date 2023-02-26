c5bf8dc5f201d97b8849d6f46226bd35439c6d9b
Martin <mart256@gmail.com>
2023-02-26 11:48:07 +0100

    Installing vim on startup

--- main.tf
+++ main.tf
@@ -16,6 +16,11 @@ resource "aws_instance" "bsd_server" {
   key_name      = "frankfurt_key"
   vpc_security_group_ids  = [ "sg-098fa48f510116f9c" ]
 
+  user_data = <<EOF
+#!/bin/sh
+pkg install vim
+EOF
+
   tags = {
     Name = "bsd_server"
   }
